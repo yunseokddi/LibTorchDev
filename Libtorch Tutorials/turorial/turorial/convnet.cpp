cee9f0dccbb71ec237258042b1ec6f7874e2dd50
yunseokddi <oys5339@naver.com>
2021-01-13 14:40:40 +0900

    NN tutorial update

---
+++ Libtorch Tutorials/turorial/turorial/convnet.cpp
@@ -0,0 +1,16 @@
+#include "convert.h"
+#include <torch/torch.h>
+
+ConvNetImpl::ConvNetImpl(int64_t num_classes) :fc(7 * 7 * 32, num_classes)
+{
+	register_module("layer1", layer1);
+	register_module("layer2", layer2);
+	register_module("fc", fc);
+}
+
+torch::Tensor ConvNetImpl::forward(torch::Tensor x) {
+    x = layer1->forward(x);
+    x = layer2->forward(x);
+    x = x.view({ -1, 7 * 7 * 32 });
+    return fc->forward(x);
+}
